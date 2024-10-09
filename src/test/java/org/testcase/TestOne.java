10c37976a8615e19885799ba57e86a6217d4c989
Ajithkumar10061998 <aajithkumar8055@gmail.com>
2024-10-09 12:43:53 +0530

    Master by Lead

---
+++ src/test/java/org/testcase/TestOne.java
@@ -0,0 +1,13 @@
+package org.testcase;
+
+public class TestOne {
+	private void one() {
+System.out.println("One");
+	}
+	private void two() {
+		System.out.println("two");
+	}
+	private void three() {
+		System.out.println("three");
+	}
+}
